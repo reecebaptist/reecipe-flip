317076384b4150759aa1193a5d349256851e2130
Reece Dsouza - r0d06w6 <reece.dsouza@walmart.com>
2025-08-14 21:20:43 +0530

    back

--- src/components/BackCoverPage.tsx
+++ src/components/BackCoverPage.tsx
@@ -5,7 +5,6 @@ const BackCoverPage: React.FC = () => {
   const year = new Date().getFullYear();
   return (
     <div className="page-content back-cover">
-      <h2 className="back-cover-title">The End</h2>
       <div className="back-cover-footer">© {year} Reece D'Souza</div>
     </div>
   );
