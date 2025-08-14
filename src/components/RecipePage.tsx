cb0fa39287852a204a9a840b6a011b455512b647
Reece Dsouza - r0d06w6 <reece.dsouza@walmart.com>
2025-08-14 18:52:12 +0530

    Scrollbar

--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -22,41 +22,45 @@ function RecipePage({
     <div className="page-content">
       <div className="recipe-container">
         <h2 className="recipe-title">{title}</h2>
-        <div className="recipe-times">
-          <div className="time-block">
-            <div className="time-title">
-              <span className="material-symbols-outlined">timer</span>
-              <strong>Prep Time</strong>
+        <div className="recipe-body">
+          <div className="recipe-times">
+            <div className="time-block">
+              <div className="time-title">
+                <span className="material-symbols-outlined">timer</span>
+                <strong>Prep Time</strong>
+              </div>
+              <div className="time-value">
+                <span>{prepTime}</span>
+              </div>
             </div>
-            <div className="time-value">
-              <span>{prepTime}</span>
+            <div className="time-block">
+              <div className="time-title">
+                <span className="material-symbols-outlined">skillet</span>
+                <strong>Cook Time</strong>
+              </div>
+              <div className="time-value">
+                <span>{cookTime}</span>
+              </div>
             </div>
           </div>
-          <div className="time-block">
-            <div className="time-title">
-              <span className="material-symbols-outlined">skillet</span>
-              <strong>Cook Time</strong>
+          <div className="recipe-details">
+            <div className="detail-header">
+              <span className="material-symbols-outlined">
+                restaurant_menu
+              </span>
+              <h3>Ingredients</h3>
             </div>
-            <div className="time-value">
-              <span>{cookTime}</span>
+            <ul className="recipe-ingredients">
+              {ingredients.map((ingredient) => (
+                <li key={ingredient}>{ingredient}</li>
+              ))}
+            </ul>
+            <div className="detail-header">
+              <span className="material-symbols-outlined">soup_kitchen</span>
+              <h3>Instructions</h3>
             </div>
+            <p className="recipe-instructions">{instructions}</p>
           </div>
-        </div>
-        <div className="recipe-details">
-          <div className="detail-header">
-            <span className="material-symbols-outlined">restaurant_menu</span>
-            <h3>Ingredients</h3>
-          </div>
-          <ul className="recipe-ingredients">
-            {ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-          <div className="detail-header">
-            <span className="material-symbols-outlined">soup_kitchen</span>
-            <h3>Instructions</h3>
-          </div>
-          <p className="recipe-instructions">{instructions}</p>
         </div>
         <div className="page-number">{pageNumber}</div>
       </div>
