c814bbb016768a7c6fc8f619d9fd7b0b7085d9a9
Reece Dsouza - r0d06w6 <reece.dsouza@walmart.com>
2025-08-14 16:26:02 +0530

    Pikachu reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,11 @@
 import React from "react";
-import HTMLFlipBook from "react-pageflip";
 import "./App.css";
-const chapters = Array.from({ length: 10 }, (_, i) => `Chapter ${i + 1}`);
+import Book from "./components/Book";
 
 const App: React.FC = () => {
-  const bookWidth = Math.floor(window.innerWidth * 0.9);
-  const bookHeight = Math.floor(window.innerHeight * 0.9);
   return (
-    <div className="App app-bg">
-      <div className="flipbook-fullscreen">
-        <HTMLFlipBook
-          usePortrait={true}
-          width={bookWidth}
-          height={bookHeight}
-          size="fixed"
-          minWidth={bookWidth}
-          maxWidth={bookWidth}
-          minHeight={bookHeight}
-          maxHeight={bookHeight}
-          maxShadowOpacity={0.25}
-          showCover={true}
-          mobileScrollSupport={true}
-          className="flipbook"
-          style={{ width: bookWidth, height: bookHeight }}
-          startPage={0}
-          drawShadow={true}
-          flippingTime={750}
-          useMouseEvents={true}
-          clickEventForward={true}
-          swipeDistance={50}
-          disableFlipByClick={false}
-          startZIndex={0}
-          autoSize={false}
-          showPageCorners={true}
-        >
-          {chapters.map((chapter, idx) => (
-            <div className="page" key={idx}>
-              <h2>{chapter}</h2>
-              <p>This is the content of {chapter}.</p>
-            </div>
-          ))}
-        </HTMLFlipBook>
-      </div>
+    <div className="container">
+      <Book />
     </div>
   );
 };
